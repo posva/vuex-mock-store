68ce763aa23d0c633e21ae852aae72c40f0502d4
igeligel <kevinigeligeligel@gmail.com>
2018-10-07 22:05:44 +0200

    invert if and return early

--- src/index.js
+++ src/index.js
@@ -30,35 +30,35 @@ exports.Store = class Store {
 
           const modules = key.split('/')
           // modules always have a trailing /
-          if (modules.length > 1) {
-            // remove trailing empty module
-            modules.pop()
-            // get the nested state corresponding to the module
-            let state = this.state
-            for (let module of modules) {
-              if (state[module]) state = state[module]
-              else {
-                throw new Error(
-                  `[vuex-mock-store] module "${key.slice(
-                    0,
-                    -1
-                  )}" not defined in state:\n${JSON.stringify(this.state, null, 2)}`
-                )
-              }
-            }
-
-            return {
-              context: {
-                // make sure we reuse this proxy
-                _modulesNamespaceMap: this._modulesNamespaceMap,
-                // pass the right state
-                state,
-                // getters are not nested
-                getters: this.getters,
-              },
-            }
-          } else {
+          if (modules.length <= 1) {
             return Reflect.get(target, key)
+          }
+
+          // remove trailing empty module
+          modules.pop()
+          // get the nested state corresponding to the module
+          let state = this.state
+          for (let module of modules) {
+            if (state[module]) state = state[module]
+            else {
+              throw new Error(
+                `[vuex-mock-store] module "${key.slice(
+                  0,
+                  -1
+                )}" not defined in state:\n${JSON.stringify(this.state, null, 2)}`
+              )
+            }
+          }
+
+          return {
+            context: {
+              // make sure we reuse this proxy
+              _modulesNamespaceMap: this._modulesNamespaceMap,
+              // pass the right state
+              state,
+              // getters are not nested
+              getters: this.getters,
+            },
           }
         },
       }
