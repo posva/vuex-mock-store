80ade7dede36c8574d57ff609a9cea116a0721d9
Kevin Peters <git.igeligel@googlemail.com>
2018-10-29 21:37:57 +0100

    chore: refactor code
    * chore: update lock file
    
    * invert if and return early
    
    * extract functionality of creating a state
    
    * chore: code style

--- src/index.js
+++ src/index.js
@@ -11,6 +11,22 @@ const defaultSpy = {
 
 exports.spy = defaultSpy
 
+function getNestedState ({ state, modules, key }) {
+  let _state = state
+  for (let module of modules) {
+    if (_state[module]) _state = _state[module]
+    else {
+      throw new Error(
+        `[vuex-mock-store] module "${key.slice(
+          0,
+          -1
+        )}" not defined in state:\n${JSON.stringify(state, null, 2)}`
+      )
+    }
+  }
+  return _state
+}
+
 exports.Store = class Store {
   constructor ({ getters = {}, state = {}, spy = defaultSpy } = {}) {
     this._spy = spy
@@ -30,35 +46,24 @@ exports.Store = class Store {
 
           const modules = key.split('/')
           // modules always have a trailing /
-          if (modules.length > 1) {
-            // remove trailing empty module
-            modules.pop()
-            // get the nested state corresponding to the module
-            let state = this.state
-            for (let module of modules) {
-              if (state[module]) state = state[module]
-              else {
-                throw new Error(
-                  `[vuex-mock-store] module "${key.slice(
-                    0,
-                    -1
-                  )}" not defined in state:\n${JSON.stringify(this.state, null, 2)}`
-                )
-              }
-            }
-
-            return {
-              context: {
-                // make sure we reuse this proxy
-                _modulesNamespaceMap: this._modulesNamespaceMap,
-                // pass the right state
-                state,
-                // getters are not nested
-                getters: this.getters,
-              },
-            }
-          } else {
+          if (modules.length <= 1) {
             return Reflect.get(target, key)
+          }
+
+          // remove trailing empty module
+          modules.pop()
+
+          const state = getNestedState({ state: this.state, modules, key })
+
+          return {
+            context: {
+              // make sure we reuse this proxy
+              _modulesNamespaceMap: this._modulesNamespaceMap,
+              // pass the right state
+              state,
+              // getters are not nested
+              getters: this.getters,
+            },
           }
         },
       }
